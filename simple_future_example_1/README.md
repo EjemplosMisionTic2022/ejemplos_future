490c4727a7a61c5d2067943ac773d5329cbf0699
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2021-08-25 17:02:17 -0500

    Update readme, organize and fix error on Future

--- simple_future_example_1/README.md
+++ simple_future_example_1/README.md
@@ -1,1 +1,4 @@
 # simple_future_example_1
+
+- Simple Future example 1 shows the use of FutureBuilder with a simple async function that returns values depending on a random variable
+  
---
+++ simple_future_example_1/lib/ui/app.dart
@@ -0,0 +1,78 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future sample 1',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilderLoader(),
+    );
+  }
+}
+
+class FutureBuilderLoader extends StatefulWidget {
+  @override
+  _FutureBuilderLoaderState createState() => _FutureBuilderLoaderState();
+}
+
+class _FutureBuilderLoaderState extends State<FutureBuilderLoader> {
+  int forceRefresh = 0;
+
+  void refresh() {
+    setState(() {
+      forceRefresh += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Future sample 1"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(onPressed: () => refresh(), child: Text('Refresh')),
+            FutureBuilder(
+                future: getData(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return Center(child: Text('DONE with ${snapshot.data}'));
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('ERROR -> ${snapshot.error}'));
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                })
+          ],
+        ));
+  }
+}
+
+Future<bool> getData() async {
+  //  <--- async function
+  print("calling getData");
+  Random random = new Random();
+  int value = random.nextInt(20);
+  await Future.delayed(Duration(seconds: 3));
+  if (value % 2 == 0) {
+    if (value <= 10) {
+      print("getData returning true");
+      return Future.value(true);
+    } else {
+      print("getData returning false");
+      return Future.value(false);
+    }
+  } else {
+    print("getData returning error");
+    return Future.error("NO ES PAR!!!");
+  }
+}
